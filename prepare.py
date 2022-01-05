8daa62fe86bb53dfa908c05bb3a197f0b457a8b5
scottschmidl <sjschmidl@gmail.com>
2022-01-05 10:50:12 -0600

    update rename of cols

--- prepare.py
+++ prepare.py
@@ -60,7 +60,7 @@ class Prepare:
         df.dropna(inplace=True)
 
         # rename columns for reability
-        cols_rename = {"parcelid": "parcel_id", "bedroomcnt": "bedroom_count", "bathroomcnt": "bathroom_count", "calculatedfinishedsquarefeet": "home_size_square_feet", "lotsizesquarefeet": "lot_size_square_feet", "regionidcity": "region_id_city", "regionidcounty": "region_id_county",  "regionidzip":"region_id_zip", "roomcnt": "room_count", "yearbuilt": "year_built", "structuretaxvaluedollarcnt": "structure_tax_value_price", "taxvaluedollarcnt": "home_price", "assessmentyear": "assessment_year", "landtaxvaluedollarcnt": "land_tax_value_price", "taxamount": "tax_amount", "logerror": "log_error", "transactiondate": "transaction_date"}
+        cols_rename = {"parcelid": "parcel_id", "bedroomcnt": "bedroom_count", "bathroomcnt": "bathroom_count", "calculatedfinishedsquarefeet": "home_size_square_feet", "lotsizesquarefeet": "lot_size_square_feet", "rawcensustractandblock":"raw_census_tract_and_block",  "regionidcity": "region_id_city", "regionidcounty": "region_id_county",  "regionidzip":"region_id_zip", "roomcnt": "room_count", "yearbuilt": "year_built", "structuretaxvaluedollarcnt": "structure_tax_value_price", "taxvaluedollarcnt": "home_price", "assessmentyear": "assessment_year", "landtaxvaluedollarcnt": "land_tax_value_price", "taxamount": "tax_amount", "censustractandblock": "census_tract_and_block", "logerror": "log_error", "transactiondate": "transaction_date"}
         df.rename(cols_rename, axis=1, inplace=True)
 
 
