d397f73d31523ef64bb883eb67eb40e0795d4e5c
scottschmidl <sjschmidl@gmail.com>
2022-01-07 14:53:14 -0600

    clust_reg: finished modeling, models: organized, sgs: added parameter to get_Xy

--- split_get_scale.py
+++ split_get_scale.py
@@ -12,20 +12,20 @@ class SplitGetScale:
 
         return train, validate, test
 
-    def get_Xy(self, train, validate, test):
+    def get_Xy(self, train, validate, test, cols_dummy):
 
         col_drop = ["parcel_id", "fips", "latitude", "longitude", "raw_census_tract_and_block", "region_id_city", "region_id_county", "region_id_zip", "structure_tax_value_price", "tax_value_dollar_count", "assessment_year", "land_tax_value_price", "census_tract_and_block", "transaction_date", "log_error"]
 
         X_train = train.drop(col_drop, axis=1)
-        X_train = pd.get_dummies(X_train, columns=["county"], drop_first=True)
+        X_train = pd.get_dummies(X_train, columns=cols_dummy, drop_first=True)
         y_train = train["log_error"]
 
         X_val = validate.drop(col_drop, axis=1)
-        X_val = pd.get_dummies(X_val, columns=["county"], drop_first=True)
+        X_val = pd.get_dummies(X_val, columns=cols_dummy, drop_first=True)
         y_val = validate["log_error"]
 
         X_test = test.drop(col_drop, axis=1)
-        X_test = pd.get_dummies(X_test, columns=["county"], drop_first=True)
+        X_test = pd.get_dummies(X_test, columns=cols_dummy, drop_first=True)
         y_test = test["log_error"]
 
         return (X_train, y_train), (X_val, y_val), (X_test, y_test)
